82e96c2ba012f85971ad86285ec9ae28415c22a8
Thomas Van <thomascvan11@gmail.com>
2023-07-18 11:13:33 -0700

    added resume download

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -4,21 +4,36 @@ import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponent
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
-const Hero = (props) => (
-  <>
-    <Section row nopadding>
-      <LeftSection>
-        <SectionTitle main center>
-          Welcome To <br />
-          My Personal Portfolio
-        </SectionTitle>
-        <SectionText>
-        The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
-        </SectionText>
-        <Button onClick={props.handleClick}>Learn More</Button>
-      </LeftSection>
-    </Section>
-  </>
-);
+const Hero = (props) => {
+
+  const handleDownloadResume = () => {
+    const resumeDownloadLink = 'https://drive.google.com/u/0/uc?id=1SzdC6qj3iYQB23hTXB7l1AapJ1V3mVU1&export=download';
+
+    const downloadLink = document.createElement('a');
+    downloadLink.href = resumeDownloadLink;
+    downloadLink.download = 'resume.pdf';
+    downloadLink.target = '_blank';
+    downloadLink.rel = 'noopener noreferrer';
+
+    downloadLink.click();
+  };
+
+  return (
+    <>
+      <Section row nopadding>
+        <LeftSection>
+          <SectionTitle main center>
+            Welcome To <br />
+            My Personal Portfolio
+          </SectionTitle>
+          <SectionText>
+          The purpose of JavaScript Mastery is to help aspiring and established developers to take their development skills to the next level and build awesome apps.
+          </SectionText>
+          <Button onClick={handleDownloadResume}>Download Resume</Button>
+        </LeftSection>
+      </Section>
+    </>
+  );
+}
 
 export default Hero;
